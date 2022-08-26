9edc7b56e9d594235ce4169452146cb60d688d64
MaxX <maxx-f@mail.ru>
2022-08-26 07:26:44 +0300

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится
+// по формуле: Amn = m+n. Выведите полученный массив на экран.
+
+void CreateMatrix(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = m + n;
+        }
+    }
+}
+void PrintMatrix(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = new int[5, 4];
+
+CreateMatrix(array);
+PrintMatrix(array);
