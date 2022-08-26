bf99fab765231e9575c139c1a3c987b8c3e7a95e
MaxX <maxx-f@mail.ru>
2022-08-26 08:25:16 +0300

    Add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса четные,
+// и замените эти элементы на их квадраты.
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max) // Задать двумерный массив
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr) // Печать двумерного массива
+{
+    for (int row = 0; row < arr.GetLength(0); row++)
+    {
+        for (int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChengeMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int column = 0; column < matrix.GetLength(1); column++)
+        {
+            if (row % 2 != 0 && column % 2 != 0) 
+            matrix[row, column] = matrix[row, column] * matrix[row, column];
+        }
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4, 0, 99);
+PrintMatrix(matrix);
+Console.WriteLine();
+ChengeMatrix(matrix);
+PrintMatrix(matrix);
